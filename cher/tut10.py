9f795fe7a47a8c12da37a99b1c25224e48107665
aalbor <aalbor@kionetworks.com>
2014-08-29 12:39:40 -0700

    Commit

--- cher/tut10.py
+++ cher/tut10.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
+import platform
 import os, os.path
 import random
 import sqlite3
-import string
+#import string
 import json
 import cherrypy
 
@@ -12,97 +14,107 @@ DB_STRING = "my.db"
 #    def index(self):
 #        return file('')
 
-#class StringGeneratorWebService(object):
-#    exposed = True
-
-
 class StringGenerator(object):
-    @cherrypy.tools.accept(media='text/plain')
-    def getcpus(request, name):
-        with sqlite3.connect(DB_STRING) as data:
-    	    cpus = get_cpus()
-    	    cputype = cpus['type']
-    	    cpucount = cpus['cpus']
-    	    data = {}
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+       <head></head>
+       <body>
+        <div class="span3">
+         <div class="widget widget-table action-table">
+            <div class="widget-header"> <i class="fa fa-dashboard"></i>
+              <h3>General Info</h3><i class="fa fa-minus"></i>
+              <div id="refresh-os">
+              </div>
+            </div>
+            <!-- /widget-header -->
+            <div class="widget-content">
+                <br>
+              <div style="text-align:center;">
+                <b>OS:</b> <span class="" id="get-osname"></span><br>
+                <b>Uptime:</b> <span class="" id="get-uptime"></span> Hours<br>
+                <b>Hostname:</b> <span class="" id="get-hostname"></span><br>
+                <b>Kernel:</b> <span class="" id="get-kernel"></span><br>
+                <b>CPU(s):</b> <span class="" id="get-cpucount"></span> x <span class="" id="get-cputype"></span>
+                <br><br>
+              </div>
+            </div>
+            <!-- /widget-content -->
+          </div>
+          <!-- /widget -->
+        </div>
+        </body>
+        </html>"""
 
-        if name == 'type':
+class StringGeneratorWebService(object):
+    exposed = True
+
+    @cherrypy.tools.accept(media='text/plain')#@login_required(login_url='/login/')
+    def platform(self, name):
+        with sqlite3.connect(DB_STRING) as c:
+            c.getplatform = get_platform()
+            c.hostname = getplatform['hostname']
+            c.osname = getplatform['osname']
+            c.kernel = getplatform['kernel']
+            c.data = {}
+
+        if name == 'hostname':
             try:
-                data = cputype
+                c.data = hostname
             except Exception:
-                data = None
+                c.data = None
 
-        if name == 'count':
+        if name == 'osname':
             try:
-                data = cpucount
+                c.data = osname
             except Exception:
-                data = None
+                c.data = None
 
-        data = json.dumps(data)
-        response = HttpResponse()
+        if name == 'kernel':
+            try:
+                c.data = kernel
+            except Exception:
+                c.data = None
+
+        c.data = json.dumps(data)
+        c.response = HttpResponse()
         #response['Content-Type'] = "text/javascript"
-        response.write(data)
-        return response
+        c.response.write(data)
+        return c.response
 
+    def get_platform():
+        with sqlite3.connect(DB_STRING) as c:
+        #Get the OS name, hostname and kernel
+            try:
+                c.osname = " ".join(platform.linux_distribution())
+                c.uname = platform.uname()
 
-def get_cpus():
-    """
-    Get the number of CPUs and model/type
-    """
-    try:
-        pipe = os.popen("cat /proc/cpuinfo |" + "grep 'model name'")
-        data = pipe.read().strip().split(':')[-1]
-        pipe.close()
+                if osname == '  ':
+                    c.osname = uname[0]
 
-        if not data:
-            pipe = os.popen("cat /proc/cpuinfo |" + "grep 'Processor'")
-            data = pipe.read().strip().split(':')[-1]
-            pipe.close()
+                c.data = {'osname': osname, 'hostname': uname[1], 'kernel': uname[2]}
 
-        cpus = multiprocessing.cpu_count()
+            except Exception, err:
+                c.data = str(err)
 
-        data = {'cpus': cpus, 'type': data}
+            return c.data
 
-    except Exception, err:
-        data = str(err)
-
-    return data
-
-
-
-def get_cpu_usage():
-    """
-    Get the CPU usage and running processes
-    """
-    try:
-        pipe = os.popen("ps aux --sort -%cpu,-rss")
-        data = pipe.read().strip().split('\n')
-        pipe.close()
-
-        usage = [i.split(None, 10) for i in data]
-        del usage[0]
-
-        total_usage = []
-
-        for element in usage:
-            usage_cpu = element[2]
-            total_usage.append(usage_cpu)
-
-        total_usage = sum(float(i) for i in total_usage)
-
-        total_free = ((100 * int(get_cpus()['cpus'])) - float(total_usage))
-
-        cpu_used = {'free': total_free, 'used': float(total_usage), 'all': usage}
-
-        data = cpu_used
-
-    except Exception, err:
-        data = str(err)
-
-    return data
 
 def setup_database():
+    """
+    Create the `user_string` table in the database
+    on server startup
+    """
     with sqlite3.connect(DB_STRING) as con:
-        con.execute("CREATE TABLE user_string (data)")
+        con.execute("CREATE TABLE user_string")
+
+def cleanup_database():
+    """
+    Destroy the `user_string` table from the database
+    on server shutdown.
+    """
+    with sqlite3.connect(DB_STRING) as con:
+        con.execute("DROP TABLE user_string")
 
 if __name__ == '__main__':
     conf = {
@@ -122,8 +134,9 @@ if __name__ == '__main__':
     }
 
     cherrypy.engine.subscribe('start', setup_database)
+    cherrypy.engine.subscribe('stop', cleanup_database)
 
     webapp = StringGenerator()
-    #webapp.generator = StringGeneratorWebService()
+    webapp.generator = StringGeneratorWebService()
     cherrypy.config.update({'server.socket_host': '192.168.241.128',})
     cherrypy.quickstart(webapp, '/', conf)
